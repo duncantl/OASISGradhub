e745408c5b1564e33f70f1978bb3a01d57f1ae23
Duncan Temple Lang <duncan@r-project.org>
2022-03-17 17:50:15 -0700

    added link to each course title to jump to SISR for that course in a separate window.

--- oasis.js
+++ oasis.js
@@ -19,7 +19,7 @@
    var sid = tmp.textContent;  // node.innerHTML;
    var a = document.createElement('a');
    a.innerHTML = node.innerHTML;  // textContent = sid;
-   a.href = "https://gradhub.ucdavis.edu/student/" + sid;
+   a.href = "https://gradhub.ucdavis.edu/student/" + sid ;
    node.innerHTML = "";
    node.prepend(a); // appendChild(a);
 
@@ -27,14 +27,91 @@
 
 // For the Academic Record page, select all the Columns by default.
 
+
+/*
+function sisr()
+{
+    alert("Hi from sisr");
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded; charset=UTF-8");
+    xhr.send(someStuff);
+}
+*/
+
+function sisr(ev)
+{
+    var form = document.forms['myform'];
+    
+    var tr = ev.target.parentNode.parentNode;
+
+    var qtr = tr.children[2].children[0];
+    console.log(qtr);
+    form.elements['termCode'].value = qtr.innerHTML; 
+
+    var ti = ev.target.title;
+    var subj = ti.replace(/ .*/, ''); 
+    var rx2 = /^[A-Z]+ ([0-9]+[A-Z]?) .*/;
+    var num = rx2.exec(ti);
+    console.log("subj: " + subj + " num " + num[1]);	     
+    form.elements['subjCode'].value = subj;
+    form.elements['course'].value = num[1];
+
+    console.log(form);
+    form.submit();
+}
+
+
+
 if(document.URL.match("courses.aspx")) {  
 
+console.log("URL: "+document.URL);
+    
    var sel = document.getElementById('ddColumns');
    
    for(var opt of sel.children) {
       opt.textContent = opt.textContent + " xxx";
       opt.selected = true;
    }
+
+    //  add links to SISR for each course.
+    // POST	https://sisr.ucdavis.edu/secure/reporting/reports/Report.cfm?reportName=StudentClassRoster
+    //    SortOrder=&SortDirection=&termCode=202010&subjCode=ANT&course=210&section=&CRNumb=&RegistrationStatus=AllReg&RegistrationStatus=WL&format=screen
+
+    var courses = document.getElementsByClassName("CourseName");
+    console.log("got courses");
+    for(var k of courses) {
+	k.addEventListener("click", sisr);
+	console.log("added link for " + k.innerHTML);
+    }
+
+
+/*
+var formString = '<form name="myform" method="post" action="https://sisr.ucdavis.edu/secure/reporting/reports/Report.cfm?reportName=StudentClassRoster"  target="_blank">
+<input type="hidden" name="termCode" value="">
+<input type="hidden" name="subjCode" value="">
+<input type="hidden" name="course" value="">
+</form>';
+*/
+    var div = document.createElement('div');
+    var f = document.createElement('FORM');
+    f.name = 'myform';
+    f.method = 'POST';
+    f.action = "https://sisr.ucdavis.edu/secure/reporting/reports/Report.cfm?reportName=StudentClassRoster" ;
+    f.target="_blank";
+
+
+    for(var v of ['termCode', 'subjCode', 'course']) {
+	var el = document.createElement('INPUT');
+	el.type = "HIDDEN";
+	el.name = v;
+	el.value = "";
+	f.appendChild(el);
+    }
+    
+    
+    div.appendChild(f);
+    document.body.appendChild(div);
+
 }
 
 
